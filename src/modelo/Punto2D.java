80701cb2cabb75a2fb800d0b9f465058784ed45e
Andres-Hidalgo7 <andres_fel.hidalgo@uao.edu.co>
2022-03-18 16:04:26 -0500

    Initial commit

---
+++ src/modelo/Punto2D.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author andre
+ */
+public class Punto2D {
+    
+    double x;
+    double y;
+    
+    public Punto2D(double x,double y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    
+    public Punto2D(){
+    
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Punto"+ x+","+y;
+    }
+    
+    
+    
+}

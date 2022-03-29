e5cf5608a46b5502a4e2b242f920cab75c9d0bab
JuanHidalgoB <juan.hidalgo@uao.edu.co>
2022-03-29 16:24:41 -0500

    Tomar Imagen y clases
    .

--- src/control/FXMLDocumentController.java
+++ src/control/FXMLDocumentController.java
@@ -5,9 +5,12 @@
  */
 package control;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.LinkedList;
 import java.util.ResourceBundle;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -17,8 +20,12 @@ import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javax.imageio.ImageIO;
 import modelo.Punto2D;
 
 /**
@@ -26,23 +33,24 @@ import modelo.Punto2D;
  * @author andres
  */
 public class FXMLDocumentController implements Initializable {
+
     //Variables para el metodo ngon
-    double coordenadaX,coordenadaY;
-    double cX,cY,o,a;
-    double[] xx,yy;
-    
+    double coordenadaX, coordenadaY;
+    double cX, cY, o, a;
+    double[] xx, yy;
+
     //variables para el metodo estrella5
-    double[] x,y;
+    double[] x, y;
     GraphicsContext g;
-    
+
     @FXML
     ColorPicker colorRelleno, colorBorde;
-    
+
     @FXML
     Label lRadio;
-    
+
     @FXML
-    Slider sliderBorde,sliderRadio;
+    Slider sliderBorde, sliderRadio;
 
     @FXML
     private Canvas lienzo;
@@ -58,58 +66,57 @@ public class FXMLDocumentController implements Initializable {
 
         System.out.println("Punto " + objp.toString());
     }
-    public void ngon(double x,double y,double r,int n){
-        if(borde.isSelected()){
+
+    public void ngon(double x, double y, double r, int n) {
+        if (borde.isSelected()) {
             g.setStroke(colorBorde.getValue());
             g.setLineWidth(sliderBorde.getValue());
         }
-            
-                
+
         xx = new double[n];
         yy = new double[n];
-        
-        a = 2 * Math.PI/n;
-        
+
+        a = 2 * Math.PI / n;
+
         o = 0;//inicializar el angulo 
-        
-        
+
         for (int i = 0; i < n; i++) {
-            
+
             cX = Math.cos(o);
             cY = Math.sin(o);
-            
-            xx[i] = x + cX*r;
-            yy[i] = y + cY*r;
-            
+
+            xx[i] = x + cX * r;
+            yy[i] = y + cY * r;
+
             o += a;
-                
+
         }
-        if(!relleno.isSelected() && borde.isSelected()){
+        if (!relleno.isSelected() && borde.isSelected()) {
             g.strokePolygon(xx, yy, n);
-        }else if(relleno.isSelected() && !borde.isSelected()){
+        } else if (relleno.isSelected() && !borde.isSelected()) {
             g.setFill(colorRelleno.getValue());
             g.fillPolygon(xx, yy, n);
-        }else{
+        } else {
             g.setFill(colorRelleno.getValue());
             g.strokePolygon(xx, yy, n);
             g.fillPolygon(xx, yy, n);
         }
-        
-        
+
     }
+
     @FXML
     private void activarBoton(ActionEvent event) {
-        if(relleno.isSelected()){
+        if (relleno.isSelected()) {
             colorRelleno.setDisable(false);
-        }else{
+        } else {
             g.setFill(colorRelleno.getValue());
             colorRelleno.setDisable(true);
         }
-        
-        if(borde.isSelected()){
-          colorBorde.setDisable(false);
-          sliderBorde.setDisable(false);
-        }else{
+
+        if (borde.isSelected()) {
+            colorBorde.setDisable(false);
+            sliderBorde.setDisable(false);
+        } else {
             colorBorde.setDisable(true);
             sliderBorde.setDisable(true);
         }
@@ -117,22 +124,22 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void hexagono(ActionEvent event) {
-        ngon(coordenadaX,coordenadaY,sliderRadio.getValue(),6);
+        ngon(coordenadaX, coordenadaY, sliderRadio.getValue(), 6);
     }
 
     @FXML
     private void heptagono(ActionEvent event) {
-        ngon(coordenadaX,coordenadaY,sliderRadio.getValue(),7);
+        ngon(coordenadaX, coordenadaY, sliderRadio.getValue(), 7);
     }
 
     @FXML
     private void octagono(ActionEvent event) {
-        ngon(coordenadaX,coordenadaY,sliderRadio.getValue(),8);
+        ngon(coordenadaX, coordenadaY, sliderRadio.getValue(), 8);
     }
 
     @FXML
     private void decagono(ActionEvent event) {
-        ngon(coordenadaX,coordenadaY,sliderRadio.getValue(),10);
+        ngon(coordenadaX, coordenadaY, sliderRadio.getValue(), 10);
     }
 
     @FXML
@@ -144,7 +151,7 @@ public class FXMLDocumentController implements Initializable {
     private void pacman(ActionEvent event) {
 
     }
-    
+
     @FXML
     private void estrella5(ActionEvent event) {
         x = new double[10];
@@ -154,50 +161,50 @@ public class FXMLDocumentController implements Initializable {
         //primer punto
         x[0] = (coordenadaX + radiointerno);
         y[0] = coordenadaY;
-        
+
         //me muevo adentro punto 2
         x[1] = (x[0] - radiointerno);
         y[1] = y[0];
-        
+
         //me dirijo al punto 3 
         x[2] = x[1] - radiointerno * Math.cos(72 * Math.PI / 180);
         y[2] = y[1] - radiointerno * Math.sin(72 * Math.PI / 180);
-        
+
         //punto 4
         x[3] = x[2] + radiointerno * Math.cos(252 * Math.PI / 180);
         y[3] = y[2] - radiointerno * Math.sin(252 * Math.PI / 180);
-        
+
         //punto 5
         x[4] = x[3] - radiointerno;
-        y[4] = y[3] ;
-        
+        y[4] = y[3];
+
         //punto 6
-        x[5]= x[4] + radiointerno * Math.cos(324 * Math.PI / 180);
-        y[5]= y[4] - radiointerno * Math.sin(324 * Math.PI / 180);
-        
+        x[5] = x[4] + radiointerno * Math.cos(324 * Math.PI / 180);
+        y[5] = y[4] - radiointerno * Math.sin(324 * Math.PI / 180);
+
         //punto 7
-        x[6]= x[5] + radiointerno * Math.cos(252 * Math.PI / 180);
-        y[6]= y[5] - radiointerno * Math.sin(252 * Math.PI / 180);
-        
+        x[6] = x[5] + radiointerno * Math.cos(252 * Math.PI / 180);
+        y[6] = y[5] - radiointerno * Math.sin(252 * Math.PI / 180);
+
         //punto 8
-        x[7]= x[6] + radiointerno * Math.cos(324 * Math.PI / 180);
-        y[7]= y[6] + radiointerno * Math.sin(324 * Math.PI / 180);
-        
+        x[7] = x[6] + radiointerno * Math.cos(324 * Math.PI / 180);
+        y[7] = y[6] + radiointerno * Math.sin(324 * Math.PI / 180);
+
         //punto 9
-        x[8]= x[7] - radiointerno * Math.cos(216 * Math.PI / 180);
-        y[8]= y[7] - radiointerno * Math.sin(216 * Math.PI / 180);
-        
+        x[8] = x[7] - radiointerno * Math.cos(216 * Math.PI / 180);
+        y[8] = y[7] - radiointerno * Math.sin(216 * Math.PI / 180);
+
         //punto 10
-        x[9]= x[0] + radiointerno * Math.cos(216 * Math.PI / 180);
-        y[9]= y[0] - radiointerno * Math.sin(216 * Math.PI / 180);
-        
-        if(!relleno.isSelected() && borde.isSelected()){
+        x[9] = x[0] + radiointerno * Math.cos(216 * Math.PI / 180);
+        y[9] = y[0] - radiointerno * Math.sin(216 * Math.PI / 180);
+
+        if (!relleno.isSelected() && borde.isSelected()) {
             g.setLineWidth(sliderBorde.getValue());
             g.strokePolygon(x, y, 10);
-        }else if(relleno.isSelected() && !borde.isSelected()){
+        } else if (relleno.isSelected() && !borde.isSelected()) {
             g.setFill(colorRelleno.getValue());
             g.fillPolygon(x, y, 10);
-        }else{
+        } else {
             g.setStroke(colorBorde.getValue());
             g.setLineWidth(sliderBorde.getValue());
             g.setFill(colorRelleno.getValue());
@@ -205,21 +212,47 @@ public class FXMLDocumentController implements Initializable {
             g.fillPolygon(x, y, 10);
         }
     }
+
     @FXML
     private void estrella6(ActionEvent event) {
 
     }
-    
+
     @FXML
     private void limpiarLienzo(ActionEvent event) {
-        g.clearRect(0, 0, lienzo.getWidth(),lienzo.getHeight());
-        
+        g.clearRect(0, 0, lienzo.getWidth(), lienzo.getHeight());
+
         g = lienzo.getGraphicsContext2D();
         double w = lienzo.getWidth();
         double h = lienzo.getHeight();
         g.setStroke(Color.BLACK);
         g.setLineWidth(0.5);
         g.strokeRect(0, 0, w, h);
+    }
+
+    @FXML
+    private void guardarIMG(ActionEvent event) throws IOException {
+
+        Image snapshot = lienzo.snapshot(null, null);
+        ImageIO.write(SwingFXUtils.fromFXImage(snapshot, null), "png", new File("canvasCaptura.png"));
+
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Guardar captura del canvas");
+
+        //Filtro para los archivos
+        fc.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("JPG", ".jpg"),
+                new FileChooser.ExtensionFilter("PNG", ".png"));
+        //Guardar la imagen
+        File canvasCapture1 = fc.showSaveDialog(null);
+        if (canvasCapture1 != null) {
+            WritableImage wi = new WritableImage((int) lienzo.getWidth(), (int) lienzo.getHeight());
+            try {
+                ImageIO.write(SwingFXUtils.fromFXImage(lienzo.snapshot(null, wi), null), "png", canvasCapture1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
@@ -230,9 +263,9 @@ public class FXMLDocumentController implements Initializable {
         g.setStroke(Color.BLACK);
         g.setLineWidth(0.5);
         g.strokeRect(0, 0, w, h);
-        
+
         colorRelleno.setDisable(true);
-        
+
     }
 
 }
---
+++ src/modelo/ColorValue.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author juank
+ */
+public class ColorValue {
+
+    private double r;
+    private double g;
+    private double b;
+    private double a;
+    
+    public ColorValue(double r, double g, double b, double a) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
+    }
+                     
+}

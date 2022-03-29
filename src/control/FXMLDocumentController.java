a1225a02d175c8787b0344ea8513a7e88d1b1cd3
Andres Hidalgo <andres_fel.hidalgo@uao.edu.co>
2022-03-29 10:46:52 -0500

    Boton de borrar

--- src/control/FXMLDocumentController.java
+++ src/control/FXMLDocumentController.java
@@ -26,13 +26,13 @@ import modelo.Punto2D;
  * @author andres
  */
 public class FXMLDocumentController implements Initializable {
+    //Variables para el metodo ngon
     double coordenadaX,coordenadaY;
-    double cX;
-    double cY;
-    double o;
-    double a;
-    double[] xx;
-    double[] yy;
+    double cX,cY,o,a;
+    double[] xx,yy;
+    
+    //variables para el metodo estrella5
+    double[] x,y;
     GraphicsContext g;
     
     @FXML
@@ -147,11 +147,79 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private void estrella5(ActionEvent event) {
+        x = new double[10];
+        y = new double[10];
 
+        double radiointerno = sliderRadio.getValue();
+        //primer punto
+        x[0] = (coordenadaX + radiointerno);
+        y[0] = coordenadaY;
+        
+        //me muevo adentro punto 2
+        x[1] = (x[0] - radiointerno);
+        y[1] = y[0];
+        
+        //me dirijo al punto 3 
+        x[2] = x[1] - radiointerno * Math.cos(72 * Math.PI / 180);
+        y[2] = y[1] - radiointerno * Math.sin(72 * Math.PI / 180);
+        
+        //punto 4
+        x[3] = x[2] + radiointerno * Math.cos(252 * Math.PI / 180);
+        y[3] = y[2] - radiointerno * Math.sin(252 * Math.PI / 180);
+        
+        //punto 5
+        x[4] = x[3] - radiointerno;
+        y[4] = y[3] ;
+        
+        //punto 6
+        x[5]= x[4] + radiointerno * Math.cos(324 * Math.PI / 180);
+        y[5]= y[4] - radiointerno * Math.sin(324 * Math.PI / 180);
+        
+        //punto 7
+        x[6]= x[5] + radiointerno * Math.cos(252 * Math.PI / 180);
+        y[6]= y[5] - radiointerno * Math.sin(252 * Math.PI / 180);
+        
+        //punto 8
+        x[7]= x[6] + radiointerno * Math.cos(324 * Math.PI / 180);
+        y[7]= y[6] + radiointerno * Math.sin(324 * Math.PI / 180);
+        
+        //punto 9
+        x[8]= x[7] - radiointerno * Math.cos(216 * Math.PI / 180);
+        y[8]= y[7] - radiointerno * Math.sin(216 * Math.PI / 180);
+        
+        //punto 10
+        x[9]= x[0] + radiointerno * Math.cos(216 * Math.PI / 180);
+        y[9]= y[0] - radiointerno * Math.sin(216 * Math.PI / 180);
+        
+        if(!relleno.isSelected() && borde.isSelected()){
+            g.setLineWidth(sliderBorde.getValue());
+            g.strokePolygon(x, y, 10);
+        }else if(relleno.isSelected() && !borde.isSelected()){
+            g.setFill(colorRelleno.getValue());
+            g.fillPolygon(x, y, 10);
+        }else{
+            g.setStroke(colorBorde.getValue());
+            g.setLineWidth(sliderBorde.getValue());
+            g.setFill(colorRelleno.getValue());
+            g.strokePolygon(x, y, 10);
+            g.fillPolygon(x, y, 10);
+        }
     }
     @FXML
     private void estrella6(ActionEvent event) {
 
+    }
+    
+    @FXML
+    private void limpiarLienzo(ActionEvent event) {
+        g.clearRect(0, 0, lienzo.getWidth(),lienzo.getHeight());
+        
+        g = lienzo.getGraphicsContext2D();
+        double w = lienzo.getWidth();
+        double h = lienzo.getHeight();
+        g.setStroke(Color.BLACK);
+        g.setLineWidth(0.5);
+        g.strokeRect(0, 0, w, h);
     }
 
     @Override
